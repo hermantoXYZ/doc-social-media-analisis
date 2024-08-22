669748f4e89f5cc69e90d73ad11c985c9781b74c
hermantoXYZ <andiupdates@gmail.com>
2024-08-22 21:02:33 +0700

    puh

---
+++ scripts.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="https://www.googletagmanager.com/gtag/js?id=G-48ERPCH5GV" id="google_gtagjs-js" async></script>
+<script type="text/javascript" id="google_gtagjs-js-after">
+/* <![CDATA[ */
+window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
+gtag("set","linker",{"domains":["www.sulselsatu.com"]});
+gtag("js", new Date());
+gtag("set", "developer_id.dZTNiMT", true);
+gtag("config", "G-48ERPCH5GV");
+/* ]]> */
+</script>
+// Script for Google Analytics
