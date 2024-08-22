56b358ac903cb96de462eee428e28884af3d04f2
hermantoXYZ <andiupdates@gmail.com>
2024-08-22 21:03:02 +0700

    puh

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 <script type="text/javascript" id="google_gtagjs-js-after">
 /* <![CDATA[ */
 window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
-gtag("set","linker",{"domains":["www.sulselsatu.com"]});
+gtag("set","linker",{"domains":["www.inviteku.com"]});
 gtag("js", new Date());
 gtag("set", "developer_id.dZTNiMT", true);
 gtag("config", "G-48ERPCH5GV");
